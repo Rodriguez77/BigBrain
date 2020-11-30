f180aa1c4d069da08cce6028eadff2308c33e3db
Rodriguez77 <jcrodriguez0247@gmail.com>
2020-11-30 14:26:29 +0800

    home page

---
+++ app/src/main/java/com/example/bigbrain/BigBrainQuestionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bigbrain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BigBrainQuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_big_brain_question)
+    }
+}
---
+++ app/src/main/java/com/example/bigbrain/CategoryAvtivity.kt
@@ -0,0 +1,11 @@
+package com.example.bigbrain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CategoryAvtivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category_avtivity)
+    }
+}
