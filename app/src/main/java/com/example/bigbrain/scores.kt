d587797f65c70d567d72b3f8e349676bac7bb481
Rodriguez77 <jcrodriguez0247@gmail.com>
2020-12-05 13:31:39 +0800

    firebase fix

--- app/src/main/java/com/example/bigbrain/scores.kt
+++ app/src/main/java/com/example/bigbrain/scores.kt
@@ -2,10 +2,9 @@ package com.example.bigbrain
 
 import java.io.Serializable
 
-class scores (var mUserName: String? = "", var correctAnswers: Int? = 0): Serializable {
+class scores (var id: String? = "", var mUserName: String? = "", var correctAnswers: Int? = 0): Serializable {
 
     override fun toString(): String {
-        return  "username: $mUserName \n\n" +
-                "score: $correctAnswers\n\n"
+        return  "Name: $mUserName \n\n" + "Score: $correctAnswers\n\n"
     }
 }
